7f3c930b8f26d1194fb3860c7315f8b32fe494de
Henry Rothenberg <hsr205@aol.com>
2022-03-04 13:52:09 -0500

    Add GetURLContents class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,34 @@
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+
 public class Main {
 
+
     public static void main(String[] args) {
-        // Code Here
+
+//      String query = "http://www.google.com/ig/calculator?hl=en&q=" + amount + curFrom + "=?" + curTo;
+//        String query = "http://www.google.com/ig/calculator?hl=en&q=1.0UnitedStatesDollar=?Euro";
+        String query = "http://www.google.com";
+
+        String output = new GetURLContents().getUrlContents(query);
+        System.out.println(output);
     }
 
+
+
+
 }
+
+
