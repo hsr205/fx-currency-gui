7a59a14ffe6f3b92f290a1fc12db2ef44984376b
Henry Rothenberg <hsr205@aol.com>
2022-03-04 14:59:47 -0500

    Change getCurrenRates() print out

--- src/main/java/GetExchangeRates.java
+++ src/main/java/GetExchangeRates.java
@@ -5,29 +5,35 @@ import java.net.URL;
 
 public class GetExchangeRates {
 
+    public static void main(String[] args) {
+        new GetExchangeRates().getCurrentRates();
+    }
+
     public void getCurrentRates() {
         try {
-            String apikey = "603b4bc0-9bf0-11ec-9e49-a14cf50a4ccb";
-            String url = "https://freecurrencyapi.net/api/v2/latest";
-            URL urlForGetRequest = new URL(url);
+            String url = "https://freecurrencyapi.net/api/v2/latest?apikey=603b4bc0-9bf0-11ec-9e49-a14cf50a4ccb";
             String readLine = null;
-            HttpURLConnection conection = (HttpURLConnection) urlForGetRequest.openConnection();
+            HttpURLConnection conection = (HttpURLConnection) new URL(url).openConnection();
             conection.setRequestMethod("GET");
             int responseCode = conection.getResponseCode();
 
             if (responseCode == HttpURLConnection.HTTP_OK) {
-                BufferedReader in = new BufferedReader(new InputStreamReader(conection.getInputStream()));
+                BufferedReader inputStream = new BufferedReader(new InputStreamReader(conection.getInputStream()));
                 StringBuffer response = new StringBuffer();
-                while ((readLine = in.readLine()) != null) {
+                while ((readLine = inputStream.readLine()) != null) {
                     response.append(readLine);
                 }
-                in.close();
-                System.out.println(response);
+                inputStream.close();
+                String responseNew = response.toString().substring(response.toString().indexOf("JPY"))
+                        .replace(",", "\n")
+                        .replaceAll("[{}\"]","");
+                System.out.println("USD To:");
+                System.out.println(responseNew);
             } else {
                 throw new Exception("Error in API Call");
             }
-        } catch (Exception ex) {
-            ex.printStackTrace();
+        } catch (Exception e) {
+            System.out.println("Exception: " + e.getMessage());
         }
     }
 
@@ -42,13 +48,13 @@ public class GetExchangeRates {
             int responseCode = conection.getResponseCode();
 
             if (responseCode == HttpURLConnection.HTTP_OK) {
-                BufferedReader in = new BufferedReader(new InputStreamReader(conection.getInputStream()));
+                BufferedReader inputSteam = new BufferedReader(new InputStreamReader(conection.getInputStream()));
                 StringBuffer response = new StringBuffer();
-                while ((readLine = in.readLine()) != null) {
+                while ((readLine = inputSteam.readLine()) != null) {
                     response.append(readLine);
                 }
-                in.close();
-                System.out.println(response.toString());
+                inputSteam.close();
+                System.out.println(response);
             } else {
                 throw new Exception("Error in API Call");
             }
@@ -61,9 +67,9 @@ public class GetExchangeRates {
         try {
             String apikey = "603b4bc0-9bf0-11ec-9e49-a14cf50a4ccb";
             String url = "https://freecurrencyapi.net/api/v2/historical?apikey=" + apikey +
-                         "&base_currency=" + currency +
-                         "&date_from=" + fromDate +
-                         "&date_to=" + toDate;
+                    "&base_currency=" + currency +
+                    "&date_from=" + fromDate +
+                    "&date_to=" + toDate;
             URL urlForGetRequest = new URL(url);
             String readLine = null;
             HttpURLConnection conection = (HttpURLConnection) urlForGetRequest.openConnection();
@@ -71,13 +77,13 @@ public class GetExchangeRates {
             int responseCode = conection.getResponseCode();
 
             if (responseCode == HttpURLConnection.HTTP_OK) {
-                BufferedReader in = new BufferedReader(new InputStreamReader(conection.getInputStream()));
+                BufferedReader inputStream = new BufferedReader(new InputStreamReader(conection.getInputStream()));
                 StringBuffer response = new StringBuffer();
-                while ((readLine = in.readLine()) != null) {
+                while ((readLine = inputStream.readLine()) != null) {
                     response.append(readLine);
                 }
-                in.close();
-                System.out.println(response.toString());
+                inputStream.close();
+                System.out.println(response);
             } else {
                 throw new Exception("Error in API Call");
             }
